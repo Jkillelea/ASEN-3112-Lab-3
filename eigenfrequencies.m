65593349666a3773999d7996fca467bc960fee6c
Jacob Killelea <Jkillelea@protonmail.ch>
2017-11-24 20:18:05 -0700

    4 element model

--- eigenfrequencies.m
+++ eigenfrequencies.m
@@ -10,9 +10,10 @@ h_R = 0.040;     % inch, rudder thickness
 E   = 10175000;  % lb/in^2 (70 GPA), modulus of elasticity
 r   = 0.0002505; % lb * sec^2/in^4 (mass density)
 
-A = w*h;
+A   = w*h;
 Izz = w*h^3/12;
 
+disp('============== 2 Element Model  ==============');
 % idk what these represent
 cm2 = r*A*L/100800;
 ck2 = 4*E*Izz/L^3;
@@ -47,28 +48,65 @@ K2 = ck2*[24  6*L   -24  6*L    0   0;
 M2 = M2(3:end, 3:end);
 K2 = K2(3:end, 3:end);
 
-f = @(omega) det(K2 - (omega^2)*M2); % eigenfrequencies are where the determinant is zero
+[v, d] = eig(M2^-1 * K2);
+% frequencies are the sqrt of the eigenvalues (sum because they're returned as diagonals of a matrix)
+d = sum(sqrt(d));
 
-% scroll through all the frequencies and figure out where the function crosses zero
-prev = 1;
-count = 0;
-freq = 0.01;
-while count < 3
-  this = f(freq);
-  if prev * this < 0 % change sign
-    fprintf('%f Hz\n', freq/(2*pi));
-    count = count + 1;
-  end
-  prev = this;
-  freq = freq + 0.005;
+for i = length(d):-1:(length(d) - 2) % lowest frequency is the last in the vector, so go in reverse
+  fprintf('Frequency %d: %f Hz\n', (length(d) - i + 1), d(i)/(2*pi));
+  fprintf('u: ');
+  disp(v(:, i));
 end
 
-% plot things
-figure; hold on; grid on;
-omega_range = 0:1350;
-prev_o = 0;
-prev_f = 0;
-fplot(f, [min(omega_range), max(omega_range)], 'linewidth', 2)
-plot(omega_range, zeros(1, length(omega_range)));
-xlabel('Frequency (radians/sec)')
-title('Determinant of K - \omega^2M')
+
+disp('============== 4 Element Model  ==============');
+% 4 element model
+cm4 = r*A*L/806400;
+ck4 = 8*E*Izz/L^3;
+
+M4 = cm4 * [77088   2916*L  23712   -1284*L  0        0        0         0       0       0;
+            2916*L  172*L^2 1284*L  -73*L^2  0        0        0         0       0       0;
+            23712   1284*L  154176  0        23712  -1284*L    0         0       0       0;
+           -1284*L -73*L^2  0       344*L^2  1284*L  -73*L^2   0         0       0       0;
+            0       0       23712   1284*L   154176   0        23712    -1284*L  0       0;
+            0       0      -1284*L -73*L^2   0        344*L^2  1284*L   -73*L^2  0       0;
+            0       0       0       0        23712    1284*L   154176    0       23712  -1284*L;
+            0       0       0       0       -1284*L  -73*L^2   0         344*L^2 1284*L -73*L^2;
+            0       0       0       0        0        0        23712     1284*L  77088  -2916*L;
+            0       0       0       0        0        0       -1284*L   -73*L^2 -2916*L  172*L^2] ...
+      + ...
+     [0       0       0       0        0        0       0         0       0       0;
+      0       0       0       0        0        0       0         0       0       0;
+      0       0       0       0        0        0       0         0       0       0;
+      0       0       0       0        0        0       0         0       0       0;
+      0       0       0       0        0        0       0         0       0       0;
+      0       0       0       0        0        0       0         0       0       0;
+      0       0       0       0        0        0       0         0       0       0;
+      0       0       0       0        0        0       0         0       0       0;
+      0       0       0       0        0        0       0         0       MT     ST;
+      0       0       0       0        0        0       0         0       ST     IT];
+
+
+K4 = ck4*[96    12*L   -96   12*L   0    0      0     0      0      0;
+          12*L  2*L^2  -12*L L^2    0    0      0     0      0      0;
+         -96   -12*L    192   0    -96   12*L   0     0      0      0;
+          12*L  L^2     0    4*L^2 -12*L L^2    0     0      0      0;
+          0     0      -96  -12*L   192  0     -96    12*L   0      0;
+          0     0       12*L L^2    0    4*L^2 -12*L  L^2    0      0;
+          0     0       0    0     -96  -12*L   192   0     -96     12*L;
+          0     0       0    0      12*L L^2    0     4*L^2 -12*L   L^2;
+          0     0       0    0      0    0     -96   -12*L   96    -12*L;
+          0     0       0    0      0    0      12*L  L^2   -12*L   2*L^2];
+
+M4 = M4(3:end, 3:end);
+K4 = K4(3:end, 3:end);
+
+[v, d] = eig(M4^-1 * K4);
+% frequencies are the sqrt of the eigenvalues (sum because they're returned as diagonals of a matrix)
+d = sqrt(sum(d))';
+
+for i = length(d):-1:(length(d) - 2) % lowest frequency is the last in the vector, so go in reverse
+  fprintf('Frequency %d: %f Hz\n', (length(d) - i + 1), d(i)/(2*pi));
+  fprintf('u: ');
+  disp(v(:, i));
+end
